432e6f77caa0d1884fe38223e93c7eda7debf29d
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-17 22:37:07 +0100

    Implement the create order command handler

--- src/main/java/com/Otto/task/command/aggregate/OrderAggregate.java
+++ src/main/java/com/Otto/task/command/aggregate/OrderAggregate.java
@@ -1,7 +1,43 @@
 package com.Otto.task.command.aggregate;
 
+import com.Otto.task.command.commands.CreateOrderCommand;
+import com.Otto.task.lib.entity.Item;
+import com.Otto.task.lib.entity.PaymentMethod;
+import com.Otto.task.lib.entity.State;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Aggregate
 public class OrderAggregate {
+
+    @AggregateIdentifier
+    private String orderNumber;
+    private String address;
+    private List<Item> items;
+    private double price;
+    private PaymentMethod paymentMethod;
+    private State state;
+    private String customerId;
+    private LocalDateTime timestamp;
+
+    public OrderAggregate() {
+    }
+
+    @CommandHandler
+    public OrderAggregate(CreateOrderCommand createOrderCommand) {
+        AggregateLifecycle.apply(new OrderCreatedEvent(
+                createOrderCommand.getId(),
+                createOrderCommand.getPaymentMethod(),
+                createOrderCommand.getAddress(),
+                createOrderCommand.getItems(),
+                createOrderCommand.getPrice(),
+                State.CREATED,
+                createOrderCommand.getCustomerId()
+        ));
+    }
 }
