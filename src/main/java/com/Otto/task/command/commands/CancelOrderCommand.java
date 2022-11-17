92da8aaee678f364b43b4abafafa49db3b3166ed
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-17 23:42:19 +0100

    Implement Cancel order command

---
+++ src/main/java/com/Otto/task/command/commands/CancelOrderCommand.java
@@ -0,0 +1,9 @@
+package com.Otto.task.command.commands;
+
+
+public class CancelOrderCommand  extends BaseCommand<String> {
+
+    public CancelOrderCommand(String id) {
+        super(id);
+    }
+}
