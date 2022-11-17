9727565a4c75bcf27288cfed16ecb9c4f0d95253
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-17 23:40:45 +0100

    Validate create order request

--- src/main/java/com/Otto/task/lib/dtos/CreateOrderRequestDTO.java
+++ src/main/java/com/Otto/task/lib/dtos/CreateOrderRequestDTO.java
@@ -6,15 +6,23 @@ import com.Otto.task.lib.entity.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class CreateOrderRequestDTO {
 
+    @NotNull(message = "Delivery address could not be null")
     private final String address;
+
     private final PaymentMethod paymentMethod;
+
+    @NotNull(message = "Cannot initialize order without items")
     private  final List<Item> items;
+
     private final double price;
+
+    @NotNull(message = "Customer Id is missing")
     private final String customerId;
 }
