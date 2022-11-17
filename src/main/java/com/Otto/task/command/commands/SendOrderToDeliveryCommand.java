ddec99ac994583b25ecd7c3a9395fe391002f3c4
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 00:54:22 +0100

    create sendOrderToDelivery command

---
+++ src/main/java/com/Otto/task/command/commands/SendOrderToDeliveryCommand.java
@@ -0,0 +1,8 @@
+package com.Otto.task.command.commands;
+
+public class SendOrderToDeliveryCommand extends BaseCommand<String>{
+
+    public SendOrderToDeliveryCommand(String id) {
+        super(id);
+    }
+}
