685b8beb90d64d6e8effbac664f44f95c490af6b
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-17 22:30:44 +0100

    Implement createOrderCommand

--- src/main/java/com/Otto/task/command/commands/CreateOrderCommand.java
+++ src/main/java/com/Otto/task/command/commands/CreateOrderCommand.java
@@ -1,2 +1,28 @@
-package com.Otto.task.command.commands;public class CreateOrderCommand {
+package com.Otto.task.command.commands;
+
+
+import com.Otto.task.lib.entity.Item;
+import com.Otto.task.lib.entity.PaymentMethod;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public final class CreateOrderCommand extends BaseCommand<String> {
+
+    private final PaymentMethod paymentMethod;
+    private final List<Item> items;
+    private final double price;
+    private final String customerId;
+    private final String address;
+
+    public CreateOrderCommand(String id, PaymentMethod paymentMethod, List<Item> items, double price, String customerId, String address) {
+        super(id);
+        this.paymentMethod = paymentMethod;
+        this.items = items;
+        this.price = price;
+        this.customerId = customerId;
+        this.address = address;
+    }
+
 }
