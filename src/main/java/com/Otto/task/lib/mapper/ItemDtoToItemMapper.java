970fb79a36dd0a00a77c5bcc6a4854a6ef33dd19
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 11:21:17 +0100

    fix logic in mapper

--- src/main/java/com/Otto/task/lib/mapper/ItemDtoToItemMapper.java
+++ src/main/java/com/Otto/task/lib/mapper/ItemDtoToItemMapper.java
@@ -4,24 +4,28 @@ import com.Otto.task.lib.dtos.ItemDTO;
 import com.Otto.task.lib.entity.Item;
 import com.Otto.task.lib.entity.Pizza;
 import com.Otto.task.lib.entity.Toppings;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class ItemDtoToItemMapper implements ItemDtoToItemMapperInterface {
 
     @Override
     public List<Item> ToItem(List<ItemDTO> itemDTOs) {
-        List<Toppings> toppings = itemDTOs.stream()
-                .map(itemDTO -> itemDTO
-                        .getToppings()
-                        .stream()
-                        .map(Toppings::toTopping)
-                        .collect(Collectors.toList())
 
-                ).toList().get(0);
+        List<Toppings> toppings = new ArrayList<>();
        return itemDTOs.stream()
-               .map(itemDTO -> new Item(Pizza.toPizza(itemDTO.getPizza()), toppings))
+               .map(itemDTO -> new Item(Pizza.toPizza(itemDTO.getPizza()),
+                      itemDTO.getToppings() != null ? itemDTO
+                               .getToppings()
+                               .stream()
+                               .map(Toppings::toTopping)
+                               .collect(Collectors.toList())
+                               : toppings
+                       ))
                .collect(Collectors.toList());
 
     }
