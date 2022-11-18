b8f8f0c59488e00bbd2b6a4967790c47e536b8f9
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 11:44:06 +0100

    Create Order Integration test

--- src/test/java/com/Otto/task/command/controller/OrderCommandControllerTest.java
+++ src/test/java/com/Otto/task/command/controller/OrderCommandControllerTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,12 +21,17 @@ class OrderCommandControllerTest {
 
     @Test
     void createOrder() throws Exception {
-        String data = "[{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLNE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
-        mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isOk());
+        String data = "{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLINE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
+        MvcResult result = mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isCreated()).andReturn();
+        System.out.println("RESULLTT: "+result.getResponse().getContentAsString());
     }
 
     @Test
-    void cancelOrder() {
+    void cancelOrder() throws Exception {
+
+        String data = "{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLINE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
+        MvcResult result = mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isCreated()).andReturn();
+        mockMvc.perform(post("/v1/order/cancel/" + result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk());
     }
 
     @Test
