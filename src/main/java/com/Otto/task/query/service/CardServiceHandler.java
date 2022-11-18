ff16695a0bab13ff1513913fa4681ffef9bd44cc
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 01:59:51 +0100

    handle OrderCanceledEvent and OrderSentToDeliveryEvent

--- src/main/java/com/Otto/task/query/service/CardServiceHandler.java
+++ src/main/java/com/Otto/task/query/service/CardServiceHandler.java
@@ -1,7 +1,9 @@
 package com.Otto.task.query.service;
 
 import com.Otto.task.lib.entity.State;
+import com.Otto.task.lib.events.OrderCanceledEvent;
 import com.Otto.task.lib.events.OrderCreatedEvent;
+import com.Otto.task.lib.events.OrderSentToDeliveryEvent;
 import com.Otto.task.query.entity.Card;
 import com.Otto.task.query.repository.CardRepository;
 import lombok.AllArgsConstructor;
@@ -20,9 +22,6 @@ public class CardServiceHandler {
     public void on(OrderCreatedEvent orderCreatedEvent) {
         log.info("OrderCreatedEvent has been triggered");
 
-        String items = orderCreatedEvent.getItems() != null ? orderCreatedEvent.getItems().stream().map(Object::toString).reduce(" ", String::concat) : null;
-
-        if (orderCreatedEvent.getItems() != null) orderCreatedEvent.getItems().forEach(System.out::println) ;
         orderRepository.save(new Card(
                 orderCreatedEvent.getId(),
                 orderCreatedEvent.getAddress(),
@@ -30,7 +29,25 @@ public class CardServiceHandler {
                 orderCreatedEvent.getPaymentMethod(),
                 State.CREATED,
                 orderCreatedEvent.getCustomerId(),
-                items
+                orderCreatedEvent.getItems().stream().map(Object::toString).reduce(" ", String::concat)
         ));
+    }
+
+    @EventHandler
+    public void on(OrderCanceledEvent orderCanceledEvent) {
+        log.info("OrderCanceledEvent has been triggered");
+
+        Card card = orderRepository.findById(orderCanceledEvent.getId()).get();
+        card.setState(State.CANCELED);
+        orderRepository.save(card);
+    }
+
+    @EventHandler
+    public void on(OrderSentToDeliveryEvent orderSentToDeliveryEvent) {
+        log.info("OrderSentToDeliveryEvent has been triggered");
+
+        Card card = orderRepository.findById(orderSentToDeliveryEvent.getId()).get();
+        card.setState(State.DELIVERED);
+        orderRepository.save(card);
     }
 }
