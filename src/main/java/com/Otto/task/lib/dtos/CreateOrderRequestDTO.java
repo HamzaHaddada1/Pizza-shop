8713cd320d5ea62ed4ea0a2dd61bc0c4d4ae21ce
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 02:00:19 +0100

    add validation on create order request

--- src/main/java/com/Otto/task/lib/dtos/CreateOrderRequestDTO.java
+++ src/main/java/com/Otto/task/lib/dtos/CreateOrderRequestDTO.java
@@ -6,6 +6,7 @@ import com.Otto.task.lib.entity.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -13,16 +14,19 @@ import java.util.List;
 @AllArgsConstructor
 public class CreateOrderRequestDTO {
 
+    @NotEmpty
     @NotNull(message = "Delivery address could not be null")
     private final String address;
 
     private final PaymentMethod paymentMethod;
 
+    @NotEmpty
     @NotNull(message = "Cannot initialize order without items")
     private  final List<Item> items;
 
     private final double price;
 
+    @NotEmpty
     @NotNull(message = "Customer Id is missing")
     private final String customerId;
 }
