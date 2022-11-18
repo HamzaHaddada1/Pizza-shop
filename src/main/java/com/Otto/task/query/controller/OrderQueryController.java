92665a6dd1dd7b593917e6143d835a245c1543ca
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 12:54:49 +0100

    change the return type for the query

--- src/main/java/com/Otto/task/query/controller/OrderQueryController.java
+++ src/main/java/com/Otto/task/query/controller/OrderQueryController.java
@@ -5,6 +5,8 @@ import com.Otto.task.query.queries.GetOrdresStateQuery;
 import lombok.AllArgsConstructor;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,18 @@ public class OrderQueryController {
     private QueryGateway queryGateway;
 
     @GetMapping("/{id}")
-    public State getOrderState(@PathVariable String id) {
+    public ResponseEntity getOrderState(@PathVariable String id) {
+        try {
+            GetOrdresStateQuery getOrdresStateQuery =
+                    new GetOrdresStateQuery(id);
 
-        GetOrdresStateQuery getOrdresStateQuery =
-                new GetOrdresStateQuery(id);
-
-        return queryGateway.query(
-                        getOrdresStateQuery,
-                        ResponseTypes.instanceOf(State.class))
-                        .join();
-
-
+            State state = queryGateway.query(
+                            getOrdresStateQuery,
+                            ResponseTypes.instanceOf(State.class))
+                    .join();
+            return new ResponseEntity(state.toString(), HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
