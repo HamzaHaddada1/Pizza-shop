40bae1a316a02df36a4e1ff76e66ba00362e4bcd
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 02:41:45 +0100

    Create the get order state query

---
+++ src/main/java/com/Otto/task/query/queries/GetOrdresStateQuery.java
@@ -0,0 +1,12 @@
+package com.Otto.task.query.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetOrdresStateQuery {
+    private String id;
+}
