bf7a41570e81a87fa826d5f458d3f9087db5fc8f
hamzahaddada <hamza.haddada@healthhero.com>
2022-11-18 12:54:10 +0100

    Test get query for all the states

--- src/test/java/com/Otto/task/query/controller/OrderQueryControllerTest.java
+++ src/test/java/com/Otto/task/query/controller/OrderQueryControllerTest.java
@@ -1,5 +1,6 @@
 package com.Otto.task.query.controller;
 
+import com.Otto.task.lib.entity.State;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,8 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -19,9 +20,28 @@ class OrderQueryControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void testGetOrderState() throws Exception {
+    void testGetOrderCreatedState() throws Exception {
         String data = "{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLINE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
         MvcResult result = mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isCreated()).andReturn();
-        mockMvc.perform(get("/v1/order/"+ result.getResponse().getContentAsString()).content(data).contentType("application/json")).andExpect(status().isOk()).andReturn();
+        result = mockMvc.perform(get("/v1/order/"+ result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk()).andReturn();
+        assertEquals(State.CREATED.toString(), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testGetOrderCanceledState() throws Exception {
+        String data = "{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLINE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
+        MvcResult result = mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isCreated()).andReturn();
+        mockMvc.perform(put("/v1/order/cancel/" + result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk());
+        result = mockMvc.perform(get("/v1/order/"+ result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk()).andReturn();
+        assertEquals(State.CANCELED.toString(), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testGetOrderDeliveredState() throws Exception {
+        String data = "{\"address\":\"SeelingStr 44\",\"price\": 12.0,\"customerId\": \"12\",\"paymentMethod\": \"ONLINE\",\"items\": [{\"pizza\": \"ROMA\"},{\"pizza\": \"NAPOLI\"},{\"pizza\": \"MARGHERITA\"}]}";
+        MvcResult result = mockMvc.perform(post("/v1/order").content(data).contentType("application/json")).andExpect(status().isCreated()).andReturn();
+        mockMvc.perform(put("/v1/order/private/deliver/" + result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk());
+        result = mockMvc.perform(get("/v1/order/"+ result.getResponse().getContentAsString()).contentType("application/json")).andExpect(status().isOk()).andReturn();
+        assertEquals(State.DELIVERED.toString(), result.getResponse().getContentAsString());
     }
 }
